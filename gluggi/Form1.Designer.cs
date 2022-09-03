cf947f4e58b5491af49c6e26474783a813b10200
PJayakrishnan <pjayakrishnan95@gmail.com>
2022-09-03 20:28:11 +0530

    First commit

---
+++ gluggi/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace gluggi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lblOption3 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.lblOption2 = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.lblOption1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblOption4 = new System.Windows.Forms.Label();
+            this.picGluggiBadge = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGluggiBadge)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblTimer);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.groupBox4);
+            this.panel1.Controls.Add(this.groupBox5);
+            this.panel1.Controls.Add(this.groupBox6);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.picGluggiBadge);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.lblScore);
+            this.panel1.Location = new System.Drawing.Point(5, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(392, 551);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblTimer.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTimer.ForeColor = System.Drawing.Color.White;
+            this.lblTimer.Location = new System.Drawing.Point(0, 0);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(24, 23);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "0";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(311, 60);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(62, 29);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lblOption3);
+            this.groupBox4.Location = new System.Drawing.Point(19, 338);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(146, 55);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            // 
+            // lblOption3
+            // 
+            this.lblOption3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOption3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblOption3.ForeColor = System.Drawing.Color.White;
+            this.lblOption3.Location = new System.Drawing.Point(3, 23);
+            this.lblOption3.Name = "lblOption3";
+            this.lblOption3.Size = new System.Drawing.Size(140, 29);
+            this.lblOption3.TabIndex = 1;
+            this.lblOption3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.lblOption2);
+            this.groupBox5.Location = new System.Drawing.Point(227, 225);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(146, 55);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            // 
+            // lblOption2
+            // 
+            this.lblOption2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOption2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblOption2.ForeColor = System.Drawing.Color.White;
+            this.lblOption2.Location = new System.Drawing.Point(3, 23);
+            this.lblOption2.Name = "lblOption2";
+            this.lblOption2.Size = new System.Drawing.Size(140, 29);
+            this.lblOption2.TabIndex = 1;
+            this.lblOption2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.lblOption1);
+            this.groupBox6.Location = new System.Drawing.Point(19, 225);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(146, 55);
+            this.groupBox6.TabIndex = 6;
+            this.groupBox6.TabStop = false;
+            // 
+            // lblOption1
+            // 
+            this.lblOption1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOption1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblOption1.ForeColor = System.Drawing.Color.White;
+            this.lblOption1.Location = new System.Drawing.Point(3, 23);
+            this.lblOption1.Name = "lblOption1";
+            this.lblOption1.Size = new System.Drawing.Size(140, 29);
+            this.lblOption1.TabIndex = 1;
+            this.lblOption1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblOption4);
+            this.groupBox3.Location = new System.Drawing.Point(224, 338);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(146, 55);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            // 
+            // lblOption4
+            // 
+            this.lblOption4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOption4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblOption4.ForeColor = System.Drawing.Color.White;
+            this.lblOption4.Location = new System.Drawing.Point(3, 23);
+            this.lblOption4.Name = "lblOption4";
+            this.lblOption4.Size = new System.Drawing.Size(140, 29);
+            this.lblOption4.TabIndex = 1;
+            this.lblOption4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picGluggiBadge
+            // 
+            this.picGluggiBadge.Image = ((System.Drawing.Image)(resources.GetObject("picGluggiBadge.Image")));
+            this.picGluggiBadge.Location = new System.Drawing.Point(-1, 460);
+            this.picGluggiBadge.Name = "picGluggiBadge";
+            this.picGluggiBadge.Size = new System.Drawing.Size(180, 90);
+            this.picGluggiBadge.TabIndex = 4;
+            this.picGluggiBadge.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(299, 485);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(71, 38);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblQuestion);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(16, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(357, 84);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblQuestion.Location = new System.Drawing.Point(3, 23);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(351, 58);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblScore.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.ForeColor = System.Drawing.Color.White;
+            this.lblScore.Location = new System.Drawing.Point(368, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(22, 23);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "0";
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(402, 561);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picGluggiBadge)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.PictureBox picGluggiBadge;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblOption3;
+        private System.Windows.Forms.Label lblOption2;
+        private System.Windows.Forms.Label lblOption1;
+        private System.Windows.Forms.Label lblOption4;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ gluggi/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gluggi
+{
+    public partial class Form1 : Form
+    {
+        int m_timerCount = 20;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CreateAnswerAndQuestion()
+        {
+            int answer = RandomNumBw1and1000();
+
+            int quesParts = RandomNumBw2and4();
+
+            string question = string.Empty;
+
+            if (quesParts == 2)
+            {
+                question = QuestionWithTwoParts(answer);
+            }
+            else if (quesParts == 3)
+            {
+                question = QuestionWithThreeParts(answer);
+            }
+            else if (quesParts == 4)
+            {
+                question = QuestionWithFourParts(answer);
+            }
+
+            this.lblQuestion.Text = question;
+        }
+
+        private string QuestionWithTwoParts(int answer)
+        {
+            return "";
+        }
+
+        private string QuestionWithThreeParts(int answer)
+        {
+            return "";
+        }
+
+        private string QuestionWithFourParts(int answer)
+        {
+            return "";
+        }
+
+        private int RandomNumBw2and4()
+        {
+            Random randum = new Random();
+            int rand_num = randum.Next(2, 4);
+            return rand_num;
+        }
+
+        private int RandomNumBw1and1000()
+        {
+            Random randum = new Random();
+            int rand_num = randum.Next(1, 1000);
+            return rand_num;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            this.btnStart.Enabled = false;
+            timer =new System.Windows.Forms.Timer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 1000;
+            timer.Start();
+
+            //CreateAnswerAndQuestion
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            m_timerCount--;
+            this.lblTimer.Text = m_timerCount.ToString();
+            if (m_timerCount == 0)
+            {
+                timer.Stop();
+                m_timerCount = 20;
+                this.btnStart.Enabled = true;
+            }
+        }
+    }
+}
